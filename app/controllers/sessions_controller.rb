eb00565abac5fa6c3fd108fe175b66a3c277c80b
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-15 13:41:28 +0300

    add login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    sessions_params = params[:session].permit(:username, :password)
+    @user = User.find_by(username: sessions_params[:username])
+    if @user && @user.authenticate(sessions_params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Login is invalid!"
+      render :new, status: :unprocessable_entity
+    end
   end
 end
