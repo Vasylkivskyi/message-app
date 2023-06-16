5d79f39dec3118b6dd14c6472aeea24c7f9fe86c
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-16 18:15:09 +0300

    change before_action

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,12 +1,8 @@
 class ChatroomController < ApplicationController
-  before_action :can_view?
+  before_action :require_user
   def index
     @messages = Message.all
   end
 
   private
-
-  def can_view?
-    redirect_to login_path if !logged_in?
-  end
 end
