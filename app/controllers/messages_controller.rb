fff036e2f29f6c4bc929baee42650c2cfcb0a3e4
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-17 09:05:01 +0300

    add message partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,22 @@
 class MessagesController < ApplicationController
+  include ActionView::Helpers::DateHelper
   def create
     @message = current_user.messages.build(message_params)
-    redirect_to root_path if @message.save
+    # http implementation
+    # redirect_to root_path if @message.save
+
+    # websocket implementation
+    if @message.save
+      ActionCable.server.broadcast "chatroom_channel",
+                                   {
+                                     message: {
+                                       body: @message.body,
+                                       created:
+                                         time_ago_in_words(@message.created_at),
+                                       user: @message.user
+                                     }
+                                   }
+    end
   end
 
   private
--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,4 +1,4 @@
-import consumer from "./consumer"
+import consumer from "./consumer";
 
 consumer.subscriptions.create("ChatroomChannel", {
   connected() {
@@ -11,5 +11,28 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-  }
+    const ol = document.getElementById("messages-list");
+    const li = document.createElement("li");
+    const { user, body, created } = data.message;
+    li.classList.add("w-2/3", "p-2", "mb-10", "rounded-lg", "shadow");
+
+    if (user.id === +ol.dataset.current_user) {
+      li.classList.add("ml-auto", "bg-slate-100");
+    }
+    li.innerHTML = `
+      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+        ${user.username}
+      </h3>
+      <p class="mb-1 text-base font-normal text-gray-500 dark:text-gray-400">
+        ${body}
+      </p>
+      <time class="mb-4 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+        ${created} ago
+      </time>
+    `;
+    ol.appendChild(li);
+    const textarea = document.getElementById("textarea");
+    textarea.value = "";
+    ol.scrollTop = ol.scrollHeight;
+  },
 });
