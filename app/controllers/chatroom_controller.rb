8712e2842707c14a606e1eff016616a682bf3384
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-17 10:11:04 +0300

    add limit to messages

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,6 +2,6 @@ class ChatroomController < ApplicationController
   before_action :require_user
   def index
     @message = Message.new
-    @messages = Message.all
+    @messages = Message.order(created_at: :asc).last(50)
   end
 end
